9c6f4fe981ecc3eb8cefc6c33df78678f65119e2
FelixBronnhuber <febrolix@gmail.com>
2025-03-04 10:16:43 +0100

    Added folke todos and snacks

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -83,4 +83,34 @@ return {
       opts.mapping = cmp.config.mapping(vim.list_extend(opts.mapping, { ["<C-[>"] = cmp.mapping.select_next_item() }))
     end,
   },
+
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
+
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      bigfile = { enabled = true },
+      dashboard = { enabled = true },
+      -- explorer = { enabled = true },
+      indent = { enabled = true },
+      input = { enabled = true },
+      -- picker = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
+  },
 }
