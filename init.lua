e479c38927151be6632e6a4f6b63dd805e2a3fe7
Felix Bronnhuber <bronfe01@thu.de>
2025-03-19 22:05:44 +0100

    Added detection for not macos regarding clipboard on wsl

--- init.lua
+++ init.lua
@@ -37,15 +37,17 @@ vim.schedule(function()
   require "mappings"
 end)
 
-vim.g.clipboard = {
-  name = "win32yank-wsl",
-  copy = {
-    ["+"] = "win32yank.exe -i --crlf",
-    ["*"] = "win32yank.exe -i --crlf",
-  },
-  paste = {
-    ["+"] = "win32yank.exe -o --lf",
-    ["*"] = "win32yank.exe -o --lf",
-  },
-  cache_enabled = true,
-}
+if vim.fn.has "macunix" ~= 1 then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = true,
+  }
+end
